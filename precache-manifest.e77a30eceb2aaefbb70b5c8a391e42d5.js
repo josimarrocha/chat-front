236000d5e47a965671c3a8e2122d0b9e827f2460
josimarrocha <rocha_josimar@yahoo.com.br>
2019-10-08 00:06:03 +0000

    Updates

---
+++ precache-manifest.e77a30eceb2aaefbb70b5c8a391e42d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54919b0607c2279e745486299ead74f3",
+    "url": "/chat-front/index.html"
+  },
+  {
+    "revision": "5ad34554ca69273fa621",
+    "url": "/chat-front/static/css/2.31956de3.chunk.css"
+  },
+  {
+    "revision": "3be1011c8c94e651ff37",
+    "url": "/chat-front/static/css/main.4ac8b572.chunk.css"
+  },
+  {
+    "revision": "5ad34554ca69273fa621",
+    "url": "/chat-front/static/js/2.cd570869.chunk.js"
+  },
+  {
+    "revision": "3be1011c8c94e651ff37",
+    "url": "/chat-front/static/js/main.98fc00ad.chunk.js"
+  },
+  {
+    "revision": "cc421bfc778c994a32a0",
+    "url": "/chat-front/static/js/runtime~main.9acc4cb0.js"
+  }
+]);
