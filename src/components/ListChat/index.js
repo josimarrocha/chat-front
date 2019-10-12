b01381a21d20c48eef149b540f2d3896fb282494
Josimar <rocha_josimar@yahoo.com.br>
2019-10-12 17:43:29 -0300

    Update component ListChat and styles

--- src/components/ListChat/index.js
+++ src/components/ListChat/index.js
@@ -1,22 +1,46 @@
-import React, { useEffect } from 'react'
-import io from 'socket.io-client'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import UserChat from './UserChat'
+import Confirm from '../Confirm'
+import { cleanPosts } from '../../reducers/posts/actionsCreators'
+import { loadingConversations } from '../../reducers/conversations/actionsCreators'
 import { ContainerList } from './styles'
 
-const ListChat = ({ listConversations, updateStatus }) => {
+const ListChat = ({ listConversations, cleanPosts, loadingConversations }) => {
+  const [isShowConfirm, setIsShowConfirm] = useState(false)
+  const [deleteUser, setDeleteUser] = useState({})
+
+  const deleteConversation = async () => {
+    await cleanPosts(deleteUser)
+    loadingConversations()
+    setIsShowConfirm(false)
+  }
   return (
-    <ContainerList>
-      <ul className="list-conversas">
-        {listConversations.contacts.map(conversation => (
-          <UserChat
-            key={conversation.user._id}
-            userChat={conversation.user}
-            lastUpdate={conversation.updatedAt}
-          />
-        ))}
-      </ul>
-    </ContainerList>
+    <>
+      {isShowConfirm && <Confirm
+        deleteConversation={deleteConversation}
+        setIsShowConfirm={setIsShowConfirm} />
+      }
+      <ContainerList>
+        <div className="content">
+          <ul className="list-conversas">
+            {listConversations.contacts.map(conversation => (
+              <UserChat
+                key={conversation.user._id}
+                userChat={conversation.user}
+                lastUpdate={conversation.updatedAt}
+                idConversation={conversation.idConversation}
+                messagesNotRead={conversation.messagesNotRead}
+                setDeleteUser={setDeleteUser}
+                setIsShowConfirm={setIsShowConfirm}
+              />
+            ))}
+          </ul>
+
+        </div>
+      </ContainerList>
+
+    </>
   )
 }
 
@@ -24,4 +48,4 @@ const mapStateToProps = state => ({
   listConversations: state.listConversations
 })
 
-export default connect(mapStateToProps)(ListChat)
+export default connect(mapStateToProps, { cleanPosts, loadingConversations })(ListChat)
--- src/components/ListChat/styles.js
+++ src/components/ListChat/styles.js
@@ -1,9 +1,26 @@
 import styled from 'styled-components'
 
-export const ContainerList = styled.div`
-  flex: 1 1 25%;
+export const ContainerList = styled.section`
+position: relative;
+flex: 1 1 25%;
+@media (max-width: 930px) {
+  /* min-width: 500px; */
+  /* flex: 0;    */
+}
+.content{
+  z-index: 5;
+  position: relative;
+  height: 100%;
   background: #ffffef;
-  height: 90%;
+  /* height: 90%; */
   overflow-y: auto;
   border-left: solid 1px #bbb;
+    /* @media (max-width: 930px) {
+      transform: translateX(110%);
+      height: 100%;
+      position: absolute;
+      right: 0;
+      bottom: 0;
+    } */
+  }
 `
