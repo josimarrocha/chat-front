fe89fd08c779f00063a549a12641a7cc619e5a5e
Josimar <rocha_josimar@yahoo.com.br>
2019-10-12 17:44:25 -0300

    Change api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,20 +1,19 @@
 import axios from 'axios'
-
-const getToken = () => {
-  const token = JSON.parse(localStorage.getItem('@chat@'))
-  if (token) {
-    return axios.create({
-      baseURL: 'http://localhost:3333',
-      headers: {
-        'Authorization': `Bearer ${token.token}`
-      }
-    })
-  } else {
-    return axios.create({
-      baseURL: 'http://localhost:3333',
-    })
-  }
-
+import { url } from './socket.io'
+const token = JSON.parse(localStorage.getItem('@chat@'))
+let api
+if (token) {
+  api = axios.create({
+    baseURL: url,
+    headers: {
+      'Authorization': `Bearer ${token.hasOwnProperty('token') ? token.token : ''}`,
+    },
+  })
+} else {
+  api = axios.create({
+    baseURL: url,
+  })
 }
 
-export default getToken()
+
+export default api
