64a6e7f9f7ae008a4e003230624043cfae9daba5
Josimar <rocha_josimar@yahoo.com.br>
2019-10-12 17:37:45 -0300

    Update component chat and styles

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,28 +1,71 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { connect } from 'react-redux'
 import io from 'socket.io-client'
+import { PulseLoader } from 'react-spinners'
 import Posts from './Posts'
-import { newMessage } from '../../reducers/posts/actionsCreators'
+import { newMessage, updateMessageRead } from '../../reducers/posts/actionsCreators'
 import { loadingConversations } from '../../reducers/conversations/actionsCreators'
 import SendMessage from './SendMessage'
-import { ContainerChat } from './styles'
+import { ContainerChat, MessageViewed } from './styles'
+import socketMessageRead, { url } from '../../config/socket.io'
 
-const Chat = ({ userActive, newMessage, loadingConversations }) => {
+let positionLine
+const Chat = ({ userActive, newMessage, loadingConversations, updateMessageRead }) => {
+  const [urlPreviewImage, setUrlPreviewImage] = useState("")
+  const [loadingMessages, setLoadingMessages] = useState(false)
+  const containerChatRef = useRef()
+  const lineRef = useRef()
+
   useEffect(() => {
-    const socket = io('http://localhost:3333')
-    socket.on('newMessage', message => {
-      newMessage(message)
-      loadingConversations()
+    positionLine = lineRef.current.getBoundingClientRect().top
+    const socket = io(url)
+    socket.on('newMessage', async message => {
+      await newMessage(message)
+      setTimeout(() => loadingConversations(), 200)
+    })
+    socketMessageRead.on('messageRead', message => {
+      updateMessageRead(message)
     })
   }, [])
+
   return (
-    <ContainerChat>
-      {userActive.hasOwnProperty('_id') && <Posts idConversation={userActive.idConversation} />}
-      {userActive.hasOwnProperty('_id') && <SendMessage />}
-    </ContainerChat>
+    <>
+      {urlPreviewImage &&
+        <div className="preview-image">
+          <div className="close-preview" onClick={() => setUrlPreviewImage(false)}>
+            <i className="fas fa-times"></i>
+          </div>
+          <figure>
+            <img src={urlPreviewImage} alt="" />
+          </figure>
+        </div>
+      }
+      <ContainerChat>
+        <div className="loader-mensagens">
+          <PulseLoader
+            loading={loadingMessages}
+            color='#7693d2'
+          />
+        </div>
+        {userActive.hasOwnProperty('_id') &&
+          <Posts
+            positionLine={positionLine}
+            idConversation={userActive.idConversation}
+            containerChatRef={containerChatRef}
+            setUrlPreviewImage={setUrlPreviewImage}
+            setLoadingMessages={setLoadingMessages}
+          />
+        }
+        <MessageViewed ref={lineRef} />
+        {userActive.hasOwnProperty('_id')
+          && <SendMessage containerChatRef={containerChatRef}
+          />
+        }
+      </ContainerChat>
+    </>
   )
 }
 const mapStateToProps = state => ({
   userActive: state.posts.userActive
 })
-export default connect(mapStateToProps, { newMessage, loadingConversations })(Chat)
+export default connect(mapStateToProps, { newMessage, loadingConversations, updateMessageRead })(Chat)
--- src/components/Chat/styles.js
+++ src/components/Chat/styles.js
@@ -2,7 +2,30 @@ import styled from 'styled-components'
 
 export const ContainerChat = styled.div`
   flex: 1 1 70%;
-  height: 90%;
+  height: 91%;
   display:flex;
   flex-direction: column;
+  position: relative;
+  background-image: url('images/speech-bubble.svg');
+  background-repeat: no-repeat;
+  background-position-x: 70px;
+
+  .loader-mensagens{
+    width: 100%;
+    position: absolute;
+    top:0;
+    left:0;
+    text-align: center;
+    background: rgba(255, 255, 255, 0.5);
+    z-index: 15;
+  }
+`
+
+export const MessageViewed = styled.div`
+  max-width: 1060px;
+  height: 30px;
+  width: 100%;
+  position: absolute;
+  bottom:75px;
+  overflow: unset
 `
