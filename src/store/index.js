49e092bb7ec2dd11fc63d72d25b558108e536f37
Josimar <rocha_josimar@yahoo.com.br>
2019-10-12 17:52:22 -0300

    Change store redux

--- src/store/index.js
+++ src/store/index.js
@@ -5,5 +5,11 @@ import reducers from '../reducers'
 const logger = () => {
   return window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 }
+let compo
+if (logger()) {
+  compo = compose(applyMiddleware(thunk), logger())
+} else {
+  compo = compose(applyMiddleware(thunk))
+}
 
-export default createStore(reducers, {}, compose(applyMiddleware(thunk)))
+export default createStore(reducers, {}, compo)
