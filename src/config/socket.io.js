ece9d58304315d7991fb877fba47e4de49dc18ac
Josimar <rocha_josimar@yahoo.com.br>
2019-10-14 11:15:20 -0300

    Change url conexao

--- src/config/socket.io.js
+++ src/config/socket.io.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
-// export const url = 'https://api-chat-back.herokuapp.com/'
-export const url = 'http://localhost:3333'
+export const url = 'https://api-chat-back.herokuapp.com/'
+// export const url = 'http://localhost:3333'
 const socket = io(url)
 
 export default socket
